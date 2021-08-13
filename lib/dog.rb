3880128fc3f45ec37ac93222a7b408f9566af08c
ellijs <ellijs@hotmail.com>
2021-08-13 16:35:33 -0400

    Completed Assignment

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,2 +1,18 @@
-class Dog
+# sql = <<-SQL
+#     CREATE TABLE IF NOT EXISTS dogs (
+#         id INTEGER PRIMARY KEY, 
+#         name TEXT,
+#         breed TEXT
+#     )
+# SQL
+
+# ActiveRecord::Base.connection.execute(sql)  
+
+# AtctiveRecord::Base.logger = Logger.new(STDOUT)
+
+
+
+
+
+class Dog < ActiveRecord::Base
 end
